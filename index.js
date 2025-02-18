41a4684d68607a69410ef9ab2f62546f6f45aac3
imagination76 <imagination767@ukr.net>
2025-02-18 22:52:03 +0200

    Update index.js

--- index.js
+++ index.js
@@ -25,10 +25,6 @@ app.get('/tasks/:id', (req, res) => {
 });
 
 // Create a new task
-
-
-
-
 app.post('/tasks', (req, res) => {
   const { title, completed } = req.body;
   if (!title) return res.status(400).send('Task title is required');
